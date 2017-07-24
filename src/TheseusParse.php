e207e88ac20d7785b159136139f51fd46f5707e5
lincanbin <lincanbin@hotmail.com>
2017-07-24 23:36:52 +0800

    Push the components up stack.

--- src/TheseusParse.php
+++ src/TheseusParse.php
@@ -8,22 +8,24 @@ class TheseusParse
 {
     public $filePath;
     public $component;
-    private $blockName;
+    public $stack;
+    private $tempComponentStack;
     public $templateBuffer;
 
 
     public function parse($fileName)
     {
         $this->filePath = dirname($fileName);
-        $this->_parse($fileName);
+        $this->component = [];
+        $this->_parse($fileName, true);
     }
 
-    private function _parse($fileName)
+    private function _parse($fileName, $isRootTemplate = false)
     {
         $handle = @fopen($fileName, "r");
         if ($handle !== false) {
             while (($line = fgets($handle, 4096)) !== false) {
-                $this->handleLine(trim($line), $fileName);
+                $this->handleLine(trim($line), $fileName, $isRootTemplate);
             }
             if (feof($handle) !== true) {
                 throw new Exception("Error: unexpected fgets() fail");
@@ -35,10 +37,17 @@ class TheseusParse
     }
 
 
-    public function handleLine($line, $fileName){
+    public function handleLine($line, $fileName, $isRootTemplate = false)
+    {
         if (preg_match("/^@import\([\'\"](.*)[\'\"]\)$/i", $line, $importFileName) > 0) {
+            $componentName = $importFileName[1];
+            if ($isRootTemplate) {
+                $this->component[$componentName] = '';
+            } else {
+                $this->tempComponentStack[$componentName] = '';
+            }
             //var_dump($importFileName);
-            $this->_parse(dirname($fileName) . '/' . $importFileName[1] . 'Component.tss');
+            $this->_parse(dirname($fileName) . '/' . $componentName . 'Component.tss');
         }
     }
 }
