25f4a5a84ed01f606ed70820fe6ef0534b242f1b
lincanbin <lincanbin@hotmail.com>
2017-07-26 21:39:44 +0800

    Test case.

--- test/index.php
+++ test/index.php
@@ -1,5 +1,8 @@
 <?php
+require __DIR__ . '/../src/HTMLElement.php';
 require __DIR__ . '/../src/TheseusParse.php';
+require __DIR__ . '/../src/TheseusDOMDocument.php';
+require __DIR__ . '/../src/TheseusDOM.php';
 require __DIR__ . '/../src/Theseus.php';
 
 use lincanbin\Theseus;
