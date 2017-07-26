9ca2428c8e007ad7b86977858cb79cc7ca059f09
lincanbin <lincanbin@hotmail.com>
2017-07-26 21:40:17 +0800

    Build up component.

--- src/Theseus.php
+++ src/Theseus.php
@@ -10,10 +10,41 @@ namespace lincanbin;
 
 class Theseus
 {
+    private $parse;
+
+    public function __construct()
+    {
+        $this->parse = new TheseusParse();
+    }
+
     public function display($fileName)
     {
-        $parse = new TheseusParse();
-        $parse->parse($fileName);
-        var_dump($parse);
+        $this->parse->parse($fileName);
+        //var_dump($this->parse);
+        $componentBuildUpList = $this->getComponentBuildUpList();
+        //var_dump($componentBuildUpList);
+        $theseusDOM = new TheseusDOM($this->parse);
+        foreach ($componentBuildUpList as $component) {
+            $this->parse->component[$component]['buffer'] = $theseusDOM->buildUp($this->parse->component[$component]['buffer']);
+        }
+        $this->parse->templateBuffer = $theseusDOM->buildUp($this->parse->templateBuffer);
+        var_dump($this->parse);
+    }
+
+    public function getComponentBuildUpList()
+    {
+        $list = [];
+        $keyGenerator = [];
+        for ($i = 0; $i <= $this->parse->maxDepth; $i++) {
+            $keyGenerator[$i] = $i * 100;
+        }
+
+        foreach ($this->parse->component as $name => $cp) {
+            $depth = $cp['depth'];
+            $list[$keyGenerator[$depth]] = $name;
+            $keyGenerator[$depth]++;
+        }
+        krsort($list);
+        return $list;
     }
 }
